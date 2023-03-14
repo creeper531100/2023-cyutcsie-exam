52bc955cfa087eb3113c72f2a5b529a469216fd5
陳村名 <creeper531100@gmail.com>
2023-03-14 18:42:33 +0800

    2在更

--- 2.cpp
+++ 2.cpp
@@ -14,7 +14,7 @@ int main() {
     cout << str << endl;
 
     stack<int> a_pos;
-    bool first_is_not_A = true;
+    bool first_is_not_A = false;
 
     //堆疊的特性是先進先出，所以抽出來的字串會是內部括號
 
@@ -29,10 +29,10 @@ int main() {
         if (str[i] == 'a' || str[i] == 'A') {
             // 如果字串開頭沒有 A 字元，那麼在檢查字元 L 是否出現時就不用檢查了。
             // 所以程式中有一個 is_first 的變數來判斷是否為第一個 A 字元。
-            first_is_not_A = false;
+            first_is_not_A = true;
             a_pos.push(i);
         }
-        else if (!first_is_not_A && (str[i] == 'l' || str[i] == 'L')) {
+        else if (first_is_not_A && (str[i] == 'l' || str[i] == 'L')) {
             // 遇到"L"或"l"且前面有"A"或"a"出現過就代表有AL字串， 
             // 就將該AL字串標為"\0"標記為無效，以便後續輸出時不會顯示出來。
             for (int j = a_pos.top(); j <= i; j++) {
